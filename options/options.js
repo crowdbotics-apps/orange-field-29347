a5a865e1d4509767cc477f74b51cb32e563f9098
Crowdbotics <team@crowdbotics.com>
2021-08-02 21:57:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orange_field_29347Identifier",
+  url: "https://orange_field_29347Identifier.botics.co",
+  api: "https://orange_field_29347Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
